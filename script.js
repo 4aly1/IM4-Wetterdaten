57e1c961e9309a3a6f31ea29c975fc79ac2febd4
marcos06101988 <MARKUS.SODO@stud.fhgr.ch>
2024-05-14 11:09:23 +0200

    Charts und beginn Design

--- script.js
+++ script.js
@@ -3,46 +3,70 @@
 // - Fetch asynchron ausführen
 
 let url = 'https://536620-3.web.fhgr.ch/php/unload.php';
-let data;
-
 
 async function fetchData(url) {
-    try {
-        let response = await fetch(url);
-        let data = await response.json();
-        return(data);
-    }
-    catch (error) {
-        console.log(error);
-    }
+  try {
+    let response = await fetch(url);
+    let dbData = await response.json();
+    return (dbData);
+  }
+  catch (error) {
+    console.log(error);
+  }
 }
 
+function renderChart(dbData, title, canvasId) {
+  let data = dbData.map(d => ({ x: d.date, y: parseFloat(d.temperature_celsius) }))
+  console.log(data)
+  let chartJsData = {
+    datasets: [
+      { data: data }
+    ]
+  };
+
+  const config = {
+    type: 'line',
+    data: chartJsData,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: title
+        }
+      }
+    },
+  };
+
+  let Wetterdaten = document.getElementById(canvasId)
+  new Chart(Wetterdaten, config);
+
+}
+
+function renderLiveData(dbData, city) {
+  let zurichNow = dbData[dbData.length - 1]
+  document.getElementById(city + "_Temperature").innerHTML = zurichNow.temperature_celsius
+  document.getElementById(city +"_ApparentTemperature").innerHTML = zurichNow.apparent_temperature
+  document.getElementById(city +"_Prec").innerHTML = zurichNow.precipitation
+  document.getElementById(city +"_Wind").innerHTML = zurichNow.wind_speed_10m
+}
 
 async function init() {
-    let response = await fetch(url);
-    data = await response.json();
-    console.log (data);
+  let dbData = await fetchData(url);
+  let zurichLast14Days = dbData
+      .filter(d => d.location === "zurich" && new Date(d.date) > new Date(Date.now() - 12096e5));
+
+  renderChart(zurichLast14Days, 'Zürich letzte 14 Tage', 'Zurich_Wetterdaten_Chart');
+  renderLiveData(zurichLast14Days, 'Zurich')
+
+  let berlinLast14Days = dbData
+      .filter(d => d.location === "berlin" && new Date(d.date) > new Date(Date.now() - 12096e5));
+
+  renderChart(berlinLast14Days, 'Berlin letzte 14 Tage', 'Berlin_Wetterdaten_Chart');
+  renderLiveData(berlinLast14Days, 'Berlin')
 }
 
 init();
-
-const Wetterdaten = document.querySelector('#Wetterdaten')
-
-new Chart(Wetterdaten, {
-  type: 'bars',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
