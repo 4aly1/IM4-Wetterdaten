0d202a35518e412915c3677508b9fd39f46a98af
marcos06101988 <MARKUS.SODO@stud.fhgr.ch>
2024-05-15 19:05:50 +0200

    Aufraumen

--- script.js
+++ script.js
@@ -4,9 +4,12 @@
 
 let url = 'https://536620-3.web.fhgr.ch/php/unload.php';
 
+//funktion erstellen für weiter unten = Daten holen von der URL und geben es mit return zurück
 async function fetchData(url) {
   try {
-    let response = await fetch(url);
+    let response = await fetch(url, {
+      mode:  'no-cors',
+    });
     let dbData = await response.json();
     return (dbData);
   }
@@ -14,7 +17,8 @@ async function fetchData(url) {
     console.log(error);
   }
 }
-
+//DBData wird übergeben inkl. title, canvas ID wo es hinkommt
+//Chart.js Teil
 function renderChart(dbData, title, canvasId) {
   let data = dbData.map(d => ({ x: d.date, y: parseFloat(d.temperature_celsius) }))
   console.log(data)
@@ -40,20 +44,20 @@ function renderChart(dbData, title, canvasId) {
       }
     },
   };
-
+//Ab hier wird die Daten ins Html geschrieben
   let Wetterdaten = document.getElementById(canvasId)
   new Chart(Wetterdaten, config);
 
 }
-
+//Aktuelle Daten werden abgefragt und ins HTML geschrieben
 function renderLiveData(dbData, city) {
   let zurichNow = dbData[dbData.length - 1]
-  document.getElementById(city + "_Temperature").innerHTML = zurichNow.temperature_celsius
+  document.getElementById(city +"_Temperature").innerHTML = zurichNow.temperature_celsius
   document.getElementById(city +"_ApparentTemperature").innerHTML = zurichNow.apparent_temperature
   document.getElementById(city +"_Prec").innerHTML = zurichNow.precipitation
   document.getElementById(city +"_Wind").innerHTML = zurichNow.wind_speed_10m
 }
-
+//Hier werden alle Datensätze abgefragt und nach gewünschten Daten gefiltert und die Funktion aufzurufen um es ins HTML zu schreiben
 async function init() {
   let dbData = await fetchData(url);
   let zurichLast14Days = dbData
